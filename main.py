dcb33eaba80424277ee42da2c53c655c307ef362
sh1shka52 <robertgavaiski@gmail.com>
2025-08-19 09:07:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from aiogram import Bot, Dispatcher
 
 from app.hendlers import router
 
+bot = Bot(token=os.getenv('token_bot').strip())
+dp = Dispatcher()
+dp.include_router(router)
 
 
-async def main():
-    bot = Bot(token='token_bot')
-    dp = Dispatcher()
-    print('...')
-    dp.include_router(router)
+async def main():  
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
